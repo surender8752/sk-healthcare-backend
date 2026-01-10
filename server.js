6d4ff2cb6c35a63379c0fc6c021860c32bdc0bb4
surender <surenderthakur40437@gmail.com>
2026-01-11 02:19:07 +0530

    Fix CORS for Vercel deployment

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 
 // api endpoints
 app.use("/api/admin", adminRouter);
